afe5b5b704fe4eecb0108ed46f97c5cb480f854b
Patrick Kozak <patrick.kozak@stiwa.com>
2025-11-21 13:50:07 +0100

    Objects2

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -14,5 +14,6 @@ public class Entity {
     public int spriteCounter = 0;
     public int spriteNum = 1;
     public Rectangle solidArea; // collision part für den player ec. für den charakter
+    public int solidAreaDefaultX, solidAreaDefaultY;
     public boolean collisionON = false;
 }
