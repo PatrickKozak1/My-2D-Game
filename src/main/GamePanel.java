af447642dde23b1a45d68d4a9255ce900f46cf16
Patrick Kozak <patrick.kozak@stiwa.com>
2026-03-16 15:39:39 +0100

    48 Lighting Effect

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import ai.PathFinder;
 import entity.Entity;
 import entity.Player;
+import enviroment.EnvironmentManager;
 import tile.TileManager;
 import tile_interactive.InteractiveTile;
 
@@ -53,6 +54,7 @@ public class GamePanel extends JPanel implements  Runnable {
     public EventHandler eHandler = new EventHandler(this);
     Config config = new Config(this);
     public PathFinder pFinder = new PathFinder(this);
+    EnvironmentManager eManager = new EnvironmentManager(this);
     Thread gameThread;
 
     // ENTITY AND OBJECT
@@ -94,6 +96,7 @@ public class GamePanel extends JPanel implements  Runnable {
         aSetter.setInteractiveTile();
 //        playMusic(1);
         gameState = titleState;
+        eManager.setup();
 
         tempScreen = new BufferedImage(screenWidth,screenHeight,BufferedImage.TYPE_INT_ARGB);
         g2 = (Graphics2D)tempScreen.getGraphics();
@@ -330,6 +333,9 @@ public class GamePanel extends JPanel implements  Runnable {
             }
             // EMPTY ENTITY LIST
             entityList.clear();
+
+            // ENVIRONMENT
+            eManager.draw(g2);
 
             // UI
             ui.draw(g2);
