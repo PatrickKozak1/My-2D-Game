cb09cb42d778b25a9e731437a9436635b00c0aca
Patrick Kozak <patrick.kozak@stiwa.com>
2026-03-17 11:14:54 +0100

    50 Day and Night Cycle

--- src/enviroment/EnvironmentManager.java
+++ src/enviroment/EnvironmentManager.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class EnvironmentManager {
 
         GamePanel gp;
-        Lighting lighting;
+        public Lighting lighting;
 
         public EnvironmentManager(GamePanel gp) {
             this.gp = gp;
--- src/main/UI.java
+++ src/main/UI.java
@@ -165,6 +165,34 @@ public class UI {
         if (gp.gameState == gp.tradeState) {
             drawTradeScreen();
         }
+
+        // SLEEP STATE
+        if (gp.gameState == gp.sleepState) {
+            drawSleepScreen();
+        }
+    }
+
+    private void drawSleepScreen() {
+
+        counter++;
+
+        if (counter < 120){
+            gp.eManager.lighting.filterAlpha += 0.01f;
+            if (gp.eManager.lighting.filterAlpha > 1f) {
+                gp.eManager.lighting.filterAlpha = 1f;
+            }
+        }
+        if (counter >= 120){
+            gp.eManager.lighting.filterAlpha -= 0.01f;
+            if (gp.eManager.lighting.filterAlpha <= 0) {
+                gp.eManager.lighting.filterAlpha = 0f;
+                counter = 0;
+                gp.eManager.lighting.dayState = gp.eManager.lighting.day;
+                gp.eManager.lighting.dayCounter = 0;
+                gp.gameState = gp.playState;
+                gp.player.getPlayerImage();
+            }
+        }
     }
 
     private void drawTradeScreen() {
